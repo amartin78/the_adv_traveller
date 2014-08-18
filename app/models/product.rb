fbfea6b04b28dd7d459307ac7e50a8c02eaf9406
Antonio <ammj24@hotmail.com>
2014-08-18 17:02:12 +0100

    MVC Currency

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ActiveRecord::Base
   validates :name, :description, :image_src, :price, :months, presence: true
   validates :name, length: { maximum: 15 }
   validates :description, length: { in: 20..200 }
-  validates :price, :format => { :with => /\A\d+(?:\.\d{0,3})?\z/ },  :numericality => { :greater_than => 0, :less_than => 10000 }
+  validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ },  :numericality => { :greater_than => 0, :less_than => 10000 }
 end
