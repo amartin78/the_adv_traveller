080bd0d814b3068978467bf85d9d77aad084db87
Antonio <ammj24@hotmail.com>
2014-09-11 18:30:54 +0100

    not performing chache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
   config.cache_store = :dalli_store, (ENV["MEMCACHIER_SERVERS"] || "").split(","),
   {
     :username => ENV["MEMCACHIER_USERNAME"], 
