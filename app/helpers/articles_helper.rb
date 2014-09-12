f65bd5b2233b83f940de99d1bea73efc9b0129e1
Antonio <ammj24@hotmail.com>
2014-09-12 12:27:06 +0100

    Cache

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,8 +1,8 @@
 module ArticlesHelper
 
 	def cache_key_for_articles
-		count = Article.count
+		# count = Article.count
 		max_updated_at = Article.maximum(:updated_at).try(:utc).try(:to_s, :number)
-		"articles/id-#{count}-#{max_updated_at}"
+		"articles/page-#{params[:page]}-#{max_updated_at}"
 	end
 end
