8280f1f5c56c2d5da04edb82c0418115eb26e08e
Antonio <ammj24@hotmail.com>
2014-09-29 15:27:23 +0100

    AJAX

--- app/assets/javascripts/payments.js.coffee
+++ app/assets/javascripts/payments.js.coffee
@@ -1,3 +1,9 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).create ->
+  $("#payments_tag").on("ajax:success", ( e, data, status, xhr ) -> 
+    $("#payments_tag").append "Successfull operation!!"
+  ).on "ajax:error", (e, xhr, status, error) -> 
+    $("#payments_tag").append "<p>ERROR</p>"
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,14 +1,24 @@
 class PaymentsController < ApplicationController
   include ActiveModel::MassAssignmentSecurity
 
+  skip_before_action :verify_authenticity_token
+
   attr_protected :customer_id
 
   def create
-    if current_user.charge(params)
-      redirect_to :back, notice: 'Product has been successfully charged.' 
-    else
-      redirect_to :back, alert: 'Product has not been charged.'
-    end
+
+  	respond_to do |format|
+	    if current_user.charge(params)
+	    	format.html { redirect_to :back, notice: 'Product has been successfully charged.' }
+	    	format.js { redirect_to :back, notice: 'Product has been successfully charged. --- js' }
+	    	format.json { render json: :back, status: :created }
+	    else
+	      format.html { redirect_to :back, alert: 'Product has not been charged.' }
+	      format.js {}
+	    	format.json { render json: :back, status: :created }
+	    end
+  	end
+
   end
 
 end
